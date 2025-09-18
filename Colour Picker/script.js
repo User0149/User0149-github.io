b1ccce83c3897edf5672406b91c0c7f804f1a7ec
User Name <evergreen9898@outlook.com>
2025-09-18 16:46:17 +1000

    UI update

--- Colour Picker/script.js
+++ Colour Picker/script.js
@@ -1,75 +1,34 @@
-let prev_rgb="rgb(0,0,0)";
-let prev_hex="#000000";
+let colours=["red","green","blue","black","white","grey","yellow","cyan","magenta","orange","brown","dark green"];
+let rgb_values=["rgb(255,0,0)","rgb(0,255,0)","rgb(0,0,255)","rgb(0,0,0)","rgb(255,255,255)","rgb(128,128,128)","rgb(255,255,0)","rgb(0,255,255)","rgb(255,0,255)","rgb(255,140,0)","rgb(139,69,19)","rgb(0,128,0)"];
+;
+let slider_table=document.getElementById("slider_table");
 
-function copy(id){
-    let val=document.getElementById(id).value;
-    navigator.clipboard.writeText(val);
-}
+for(let i=0;i<=colours.length-1;i++){
+    let c_name=colours[i];
+    let rgb_code=rgb_values[i];
 
-function hide_preview(){
-    let preview=document.getElementById("colour_preview_box");
-    preview.style.display="none";
-}
-function preview(code){
-    let preview=document.getElementById("colour_preview_box");
-    preview.style.backgroundColor=code;
-    preview.style.display='block';
-}
+    let new_row=slider_table.insertRow(-1);
+    let cell1=new_row.insertCell(-1);
+    let cell2=new_row.insertCell(-1);
+    new_row.classList.add("slider_row");
+    cell1.classList.add("table_colour");
 
-function valid_rgb(code){
-    if(code.slice(0,4)!="rgb(") return 0;
-    if(code[code.length-1]!=")") return 0;
-    code=code.slice(4,code.length-1);
-    let rgb=code.split(",");
-    if(rgb.length!=3) return 0;
-    if(!(0<=+rgb[0] && +rgb[0]<=255)) return 0;
-    if(!(0<=+rgb[1] && +rgb[1]<=255)) return 0;
-    if(!(0<=+rgb[2] && +rgb[2]<=255)) return 0;
-    return 1;
-}
-function valid_hex(code){
-    if(code.length!=7) return 0;
-    if(code[0]!='#') return 0;
-    if(!(('0'<=code[1] && code[1]<='9') || ('a'<=code[1] && code[1]<='f'))) return 0;
-    if(!(('0'<=code[2] && code[2]<='9') || ('a'<=code[2] && code[2]<='f'))) return 0;
-    if(!(('0'<=code[3] && code[3]<='9') || ('a'<=code[3] && code[3]<='f'))) return 0;
-    if(!(('0'<=code[4] && code[4]<='9') || ('a'<=code[4] && code[4]<='f'))) return 0;
-    if(!(('0'<=code[5] && code[5]<='9') || ('a'<=code[5] && code[5]<='f'))) return 0;
-    if(!(('0'<=code[6] && code[6]<='9') || ('a'<=code[6] && code[6]<='f'))) return 0;
-    return 1;
-}
+    let colour_container=document.createElement("div");
+    colour_container.classList.add("flex","flex_align_center");
+    cell1.appendChild(colour_container);
 
-function try_rgb_preview(rgb_code){
-    if(valid_rgb(rgb_code)) preview(rgb_code);
-    else hide_preview();
-}
-function try_hex_preview(hex_code){
-    if(valid_hex(hex_code)) preview(hex_code);
-    else hide_preview();
-}
+    let colour_name=document.createElement("div");
+    colour_name.classList.add("courier_new","flex_float_right");
+    colour_name.innerText=c_name;
+    colour_container.appendChild(colour_name);
 
-function set_colour(rgb_code){ // TODO: update square, update rgb and hex values, update gradients
-}
-
-function try_set_rgb(rgb_code){
-    if(valid_rgb(rgb_code)){
-        hide_preview();
-        set_colour(rgb_code);
-    }
-    else{
-        hide_preview();
-        let rgb_value=document.getElementById("rgb_value");
-        rgb_value.value=prev_rgb;
-    }
-}
-function try_set_hex(hex_code){
-    if(valid_hex(hex_code)){
-        hide_preview();
-        set_colour(hex_to_rgb(hex_code));
-    }
-    else{
-        hide_preview();
-        let hex_value=document.getElementById("hex_value");
-        hex_value.value=prev_hex;
-    }
+    let colour_preview=document.createElement("div");
+    colour_preview.classList.add("table_colour_preview","flex_float_right");
+    colour_preview.style.backgroundColor=rgb_code;
+    colour_container.appendChild(colour_preview);
+    
+    let slider=document.createElement("div");
+    slider.classList.add("slider","black_border");
+    slider.style.backgroundImage=`linear-gradient(to right,rgb(0,0,0),${rgb_code})`;
+    cell2.appendChild(slider);
 }
